0cb844044a684d00ccd23dc5289b4e652f6baf21
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-01-04 11:24:24 +0530

    routes created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='mx-4 sm:max-[10%]' >
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route  path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/"docId' element={<Appointment />} />
+      </Routes>
+    </div>
   )
 }
 

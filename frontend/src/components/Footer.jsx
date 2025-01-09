560160bee8258418dffb7812637298becc282984
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-01-09 22:13:19 +0530

    updated footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,43 +1,61 @@
-import React from 'react'
-import {assets} from '../assets/assets_frontend/assets'
+import React from 'react';
+import { assets } from '../assets/assets_frontend/assets';
+import { Mail, Phone } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <div className='md:mx-10'>
-        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'> 
+    <footer className="bg-gray-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12">
+          {/* Company Info */}
+          <div className="col-span-1 lg:col-span-2">
+            <img className="h-12 w-auto mb-6" src={assets.logo} alt="Prescripto" />
+            <p className="text-gray-600 leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In exercitationem repellat optio magni id. Minima, unde! Assumenda ducimus sunt illum corporis molestiae.
+            </p>
+          </div>
 
-            <div>
-              <img className='mb-5 w-40' src={assets.logo} alt="" />
-              <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In exercitationem repellat optio magni id. Minima, unde! Assumenda ducimus sunt illum corporis molestiae modi reprehenderit officiis odit! Totam esse quos laudantium!</p>
+          {/* Company Links */}
+          <div>
+            <h3 className="text-gray-900 font-semibold text-lg mb-4">Company</h3>
+            <ul className="space-y-3">
+              {['Home', 'About Us', 'Contact Us', 'Privacy Policy'].map((item) => (
+                <li key={item}>
+                  <a href="#" className="text-gray-600 hover:text-blue-600 transition-colors duration-200">
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-            </div>
-               <div>
-                <p className='text-xl font-medium mb-5'>COMPANY</p>
-                <ul className='flex flex-col gap-2 text-gray-600'>
-                  <li>Home</li>
-                  <li>About Us</li>
-                  <li>Contact Us</li>
-                  <li>Privacy Policy</li>
-                </ul>
-
-               </div>
-
-               <div>
-                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-                <ul className='flex flex-col gap-2 text-gray-600'>
-                  <li>+1 39054 95443</li>
-                  <li>prescripto@gmail.com</li>
-                </ul>
-                
-               </div>
+          {/* Contact Info */}
+          <div>
+            <h3 className="text-gray-900 font-semibold text-lg mb-4">Get in Touch</h3>
+            <ul className="space-y-3">
+              <li className="flex items-center text-gray-600">
+                <Phone className="h-5 w-5 mr-2 text-blue-600" />
+                <span>+1 390 5495 443</span>
+              </li>
+              <li className="flex items-center text-gray-600">
+                <Mail className="h-5 w-5 mr-2 text-blue-600" />
+                <span>prescripto@gmail.com</span>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <hr />
-          <p className='py-5 text-sm text-center '>Copyright 2025@ Prescripto-All Right Reserved.</p>
+      </div>
+
+      {/* Bottom Bar */}
+      <div className="border-t border-gray-200">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <p className="py-6 text-sm text-center text-gray-600">
+            Copyright © 2025 Prescripto - All Rights Reserved
+          </p>
         </div>
+      </div>
+    </footer>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Footer
+export default Footer;

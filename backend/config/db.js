2a3f1705815d02d23d9f1ca773961787a265b8bf
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-01-12 12:13:00 +0530

    database connection

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const connectDB=async()=>{
+
+    
+  await mongoose.connect(process.env.MONGODB_URI)
+  .then(()=>{
+    console.log("Database connected");
+  }).catch((error)=>{
+    console.log(error);
+  })
+}
+
+
+export default connectDB;

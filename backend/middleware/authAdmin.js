6622965fd59e3f793e4f24ad1d98bf9174cc28b6
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-01-22 11:52:40 +0530

    admin middleware

---
+++ backend/middleware/authAdmin.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+
+const authAdmin=async(req,res,next)=>{
+    try {
+        const {adminToken}=req.headers;
+        if(!adminToken){
+            return res.status(401).json({
+                success:false,
+                message:"Unauthorized"
+            })
+        }
+
+        const token_decode=jwt.verify(adminToken,process.env.JWT_SECRET);
+        if(token_decode!==process.env.ADMIN_EMAIL+process.env.ADMIN_PASSWORD){
+            return res.status(401).json({
+                success:false,
+                message:"Unauthorized"
+            })
+        }
+
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+        
+    }
+
+}
+
+
+e
+export default authAdmin;

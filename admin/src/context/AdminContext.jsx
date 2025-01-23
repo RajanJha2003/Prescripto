44fb32e784b812afc642fc76c55f82edbd29b3ff
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-01-23 10:32:58 +0530

    context providers added

---
+++ admin/src/context/AdminContext.jsx
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+
+
+export const AdminContext=createContext();
+
+
+
+const AdminContextProvider = (props) => {    
+
+    const values={
+         
+    }
+
+    return(
+        <AdminContext.Provider value={values}>
+            {props.children}
+        </AdminContext.Provider>
+    )
+
+    
+
+}
+
+
+export default AdminContextProvider;
--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -8,9 +8,18 @@ export const AppContext=createContext();
 const AppContextProvider = (props) => {    
 
     const values={
-        
+         
     }
+
+    return(
+        <AppContext.Provider value={values}>
+            {props.children}
+        </AppContext.Provider>
+    )
 
     
 
 }
+
+
+export default AppContextProvider;

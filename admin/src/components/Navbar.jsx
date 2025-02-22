e8063ab39bbd03cccb304843743881c02be833ab
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-02-22 16:49:08 +0530

    added admin pages

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,35 +1,38 @@
-import React, { useContext } from 'react'
-import {assets} from '../assets/assets_admin/assets'
-import { AdminContext } from '../context/AdminContext'
-
-import {useNavigation} from 'react-router-dom'
+import React, { useContext } from "react";
+import { assets } from "../assets/assets_admin/assets";
+import { AdminContext } from "../context/AdminContext";
 
 const Navbar = () => {
-    const {adminToken,setAdminToken}=useContext(AdminContext);
+  const { adminToken, setAdminToken } = useContext(AdminContext);
 
-    const navigate=useNavigate();
-
-
-    const logout=()=>{
-        navigate('/')
-        adminToken && setAdminToken('')
-        adminToken && localStorage.removeItem('adminToken')
+  const logout = () => {
+    if (adminToken) {
+      setAdminToken("");
+      localStorage.removeItem("adminToken");
     }
+  };
+
   return (
-    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white' >
-        <div className='flex items-center gap-2 text-xs '>
-            <img className='w-26 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
-            <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600 '>
-                {adminToken? 'Admin':'Doctor'}
-
-            </p>
-        </div>
-        <button onClick={logout} className='bg-[#5f6FFF] text-white px-10 py-2 rounded-full '>
-            Logout
-        </button>
-
+    <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white">
+      <div className="flex items-center gap-2">
+        <img
+          className="w-26 sm:w-40"
+          src={assets.admin_logo}
+          alt=""
+        />
+        <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">
+          {adminToken ? "Admin" : "Doctor"}
+        </p>
+      </div>
+      
+      <button
+        onClick={logout}
+        className="bg-blue-600 text-white px-10 py-2 rounded-full hover:bg-blue-700 transition-colors"
+      >
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
